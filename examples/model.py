2ee9d85eb0a39f469ee4b304ab29d042d5fd67aa
agerardin <antoine.gerardin@gmail.com>
2024-01-30 04:17:25 -0500

    experiment: check which setter we need

--- examples/model.py
+++ examples/model.py
@@ -3,8 +3,8 @@ class Process():
 
 
 class IO():
-    def __init__(self, step, name, value):
-        self.step = step
+    def __init__(self, process, name, value):
+        self.process = process
         self.name = name
         if isinstance(value,IO):
             print("!!!!!!!! linking to an IO")
@@ -12,7 +12,8 @@ class IO():
         self.value = value
 
     def can_link(self, other):
-        print("check if we can link input and output")
+        print("####### check if we can link input and output")
+
 
 
 class Process():
@@ -31,44 +32,65 @@ class Process():
         io = IO(self, name, value)
         self.ios[name] = io
 
-    def __getattribute__(self, name):
+    def __getattr__(self, name):
         if name == "ios":
-            return super().__getattribute__(name)
+            return super().__getattr__(name)
         else:
             io = self.ios.get(name)
             if not io:
                 raise AttributeError(f"could not find io with name : {name}")
+            if isinstance(io.value, IO):
+                print(f"this is a linked IO. {name} : {io.value.name} value : {io.value.value}")
             return io
 
 
-class Step():
+class Step(Process):
     def __init__(self):
         super().__init__()  
 
-    def __getattribute__(self, name):
-        return super().__getattribute__(name)
-
-    def __setattr__(self, name, value):
-        super().__setattr__(name, value)
+    def __getattr__(self, name):
+        return super().__getattr__(name)
 
 
-class Workflow():
+class Workflow(Process):
     def __init__(self):
         super().__init__()
 
-    def __getattribute__(self, name):
-        return super().__getattribute__(name)
+    def __getattr__(self, name):
+        if name == "generate":
+            return self.generate()
+        return super().__getattr__(name)
 
     def __setattr__(self, name, value):
         super().__setattr__(name, value)
 
+    def generate(self):
+        print("generate workflow")
+        if not workflow.ios:
+            raise Exception("empty workflow")
+        else:
+            for io in workflow.ios:
+                print(io)
+
+
 step1 = Step()
 step1._out = "Hello"
+step1._in = 23
 
 step2 = Step()
 step2._in = step1._out
 
 step2._in = "bad"
+step2._out = "result"
 
+
+workflow = Workflow()
+workflow._image_path = step1._in
+
+workflow.out = step2._out
 
 print(step2._in.value)
+print(workflow.out.value)
+
+
+workflow.generate()
