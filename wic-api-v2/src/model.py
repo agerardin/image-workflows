e0dd859b9b9771de3f47006c550b50dbd9284a40
agerardin <antoine.gerardin@gmail.com>
2024-02-11 04:07:36 -0500

    dev: remove dataclass in favor of base model. Test clt serialization.

--- wic-api-v2/src/model.py
+++ wic-api-v2/src/model.py
@@ -1,5 +1,5 @@
 from typing import Union
-from pydantic import BaseModel, ConfigDict, Field
+from pydantic import BaseModel, ConfigDict, Field, ValidationError
 import cwl_utils.parser as cwl_parser
 from pathlib import Path
 from yaml import safe_load, dump
@@ -16,32 +16,27 @@ Id = NewType("Id", str)
 class ProcessRequirement(BaseModel):
     class_: str = Field(..., alias='class')   
 
-@dataclass
 class SubworkflowFeatureRequirement(ProcessRequirement):
     pass
 
-class SoftwareRequirement:
+class SoftwareRequirement(ProcessRequirement):
     pass
 
+class DockerRequirement(ProcessRequirement):
+    pass 
 
-@dataclass
-class Process():
-    id: str
-
-@dataclass
-class InputBinding:
+class InputBinding(BaseModel):
     pass
 
 # TODO CHANGE for now stick to cwl_parser
 # but should be CLTInputBinding
-@dataclass
-class CommandLineBinding(InputBinding):
+class CommandLineBinding(InputBinding, extra='ignore'):
     position: Optional[int] = None
 
 # TODO CHANGE for now stick to cwl_parser
 # but should be CLTOutputBinding
-@dataclass
-class CommandOutputBinding:
+
+class CommandOutputBinding(BaseModel):
     glob: Optional[str]
     loadContents: Optional[bool]
     outputEval: Optional[str]
@@ -72,7 +67,6 @@ class CommandInputParameter(InputParameter):
 class CommandOutputParameter(OutputParameter):
     outputBinding: Optional[CommandOutputBinding] = None
 
-
 class WorkflowStepInput(BaseModel):
     id: str
     source: str
@@ -99,7 +93,9 @@ class WorkflowStep(BaseModel):
     out: list[WorkflowStepOutput]
     from_builder: Optional[bool] = False
 
-@dataclass
+class Process(BaseModel):
+    id: Id
+
 class Workflow(Process):
     inputs: list[WorkflowInputParameter]
     outputs: list[WorkflowOutputParameter]
@@ -112,27 +108,23 @@ class Workflow(Process):
     #inputs: dict[Id, WorkflowInputParameter]
     #outputs: dict[Id, WorkflowOutputParameter]
 
-@dataclass
 class CommandLineTool(Process):
-   id : Id
-   baseCommand: str
-   inputs: list[CommandInputParameter]
-   outputs: list[CommandOutputParameter]
-   # TODO maybe have transformation to store dict
-   # but only if we don't need ordering
-   # inputs: dict[Id, CommandInputParameter]
-   # outputs: dict[Id, CommandOutputParameter]
-   class_: str = 'CommandLineTool'
-   cwlVersion: str = "v1.2"
-   stdout: Optional[str] = None
-   doc: Optional[str] = ""
-   label: Optional[str] = ""
+    model_config = ConfigDict(extra='ignore')
+    baseCommand: str
+    inputs: list[CommandInputParameter]
+    outputs: list[CommandOutputParameter]
+    # TODO maybe have transformation to store dict
+    # but only if we don't need ordering
+    #inputs: dict[Id, CommandInputParameter]
+    #outputs: dict[Id, CommandOutputParameter]
+    class_: Optional[str] = Field(..., alias='class') 
+    cwlVersion: Optional[str] = "v1.2"
+    stdout: Optional[str] = None
+    doc: Optional[str] = ""
+    label: Optional[str] = ""
 
 class ExpressionTool:
     pass
-
-class DockerRequirement:
-    pass 
 
 class Operation:
     pass
@@ -146,160 +138,176 @@ with cwl_file.open("r", encoding="utf-8") as file:
 # NOTE we use the cwl_parser to standardize the representation
 cwl_clt = cwl_parser.load_document_by_uri(cwl_file)
 yaml_clt = cwl_parser.save(cwl_clt)
+
+try:
+    clt = CommandLineTool(**yaml_clt) 
+    print(clt)
+except ValidationError as e:
+    print(e.errors())
+
 clt = CommandLineTool(**yaml_clt)
-print(clt)
+path = Path() / "serialized_clt.cwl"
+serialized_clt = clt.model_dump(by_alias=True)
+print(dump(serialized_clt))
+with open(path, "w", encoding="utf-8") as file:
+    file.write(dump(serialized_clt))
 
-workflow_file= Path("/Users/antoinegerardin/Documents/projects/polus-pipelines/wic-api-v2/tests/test-data/cwl/workflow5.cwl")
-cwl_wf1 = cwl_parser.load_document_by_uri(workflow_file)
-yaml_wf1 = cwl_parser.save(cwl_wf1)
-wf1 = Workflow(**yaml_wf1)
-print(wf1)
 
-subworkflow_file = Path("/Users/antoinegerardin/Documents/projects/polus-pipelines/wic-api-v2/tests/test-data/cwl/subworkflow1.cwl")
-cwl_wf2 = cwl_parser.load_document_by_uri(subworkflow_file)
-yaml_wf2 = cwl_parser.save(cwl_wf2)
-wf2 = Workflow(**yaml_wf2)
-print(wf2)
+print(serialized_clt)
 
-class StepBuilder():
-    """Builder for a step object.
+
+# workflow_file= Path("/Users/antoinegerardin/Documents/projects/polus-pipelines/wic-api-v2/tests/test-data/cwl/workflow5.cwl")
+# cwl_wf1 = cwl_parser.load_document_by_uri(workflow_file)
+# yaml_wf1 = cwl_parser.save(cwl_wf1)
+# wf1 = Workflow(**yaml_wf1)
+# print(wf1)
+
+# subworkflow_file = Path("/Users/antoinegerardin/Documents/projects/polus-pipelines/wic-api-v2/tests/test-data/cwl/subworkflow1.cwl")
+# cwl_wf2 = cwl_parser.load_document_by_uri(subworkflow_file)
+# yaml_wf2 = cwl_parser.save(cwl_wf2)
+# wf2 = Workflow(**yaml_wf2)
+# print(wf2)
+
+# class StepBuilder():
+#     """Builder for a step object.
     
-    Create a step from a clt.
-    Each inputs/outputs of the clt are instantiated as step in/out.
-    When the final step is returned, 
-    - unset inputs are removed.
-    - outputs which are source of another step or outputSource of 
-    workflow step are removed.
-    """
+#     Create a step from a clt.
+#     Each inputs/outputs of the clt are instantiated as step in/out.
+#     When the final step is returned, 
+#     - unset inputs are removed.
+#     - outputs which are source of another step or outputSource of 
+#     workflow step are removed.
+#     """
 
-    def __init__(self, clt : Process):
-        # TODO REVIEW tentative
-        id = "step_"+ Path(clt.id).stem
-        run = clt.id
-        inputs = [{"id":input.id, "source":"UNSET", "type": input.type} for input in clt.inputs]
-        outputs = [output.id for output in clt.outputs]
-        self.step = WorkflowStep(
-            id = id,
-            run = run,
-            in_ = inputs,
-            out = outputs,
-            from_builder = True
-            )
+#     def __init__(self, clt : Process):
+#         # TODO REVIEW tentative
+#         id = "step_"+ Path(clt.id).stem
+#         run = clt.id
+#         inputs = [{"id":input.id, "source":"UNSET", "type": input.type} for input in clt.inputs]
+#         outputs = [output.id for output in clt.outputs]
+#         self.step = WorkflowStep(
+#             id = id,
+#             run = run,
+#             in_ = inputs,
+#             out = outputs,
+#             from_builder = True
+#             )
 
-    def __call__(self) -> WorkflowStep:
-        return self.step
+#     def __call__(self) -> WorkflowStep:
+#         return self.step
 
-class WorkflowBuilder():
-    """Builder for a workflow object.
+# class WorkflowBuilder():
+#     """Builder for a workflow object.
     
-    Enable iteratively to create a workflow.
-    """
-    def __init__(self, id: str, *args: Any, **kwds: Any):
-        kwds.setdefault("steps", [])
-        # collect all step inputs create a workflow input for each
-        # collect all step outputs and create a workflow output for each
-        # NOTE for now each output from each step creates an output
-        # TODO make this optional
-        # TODO we could also reduced workflow outputs to only those 
-        # which are not already connected.
-        inputs = []
-        outputs = []
-        for step in kwds.get("steps"):
-            step_inputs = [{"id": id + "/" + step.id + "/" + input.id, "type":input.type} for input in step.in_ if input.source == 'UNSET']
-            inputs = inputs + step_inputs
+#     Enable iteratively to create a workflow.
+#     """
+#     def __init__(self, id: str, *args: Any, **kwds: Any):
+#         kwds.setdefault("steps", [])
+#         # collect all step inputs create a workflow input for each
+#         # collect all step outputs and create a workflow output for each
+#         # NOTE for now each output from each step creates an output
+#         # TODO make this optional
+#         # TODO we could also reduced workflow outputs to only those 
+#         # which are not already connected.
+#         inputs = []
+#         outputs = []
+#         for step in kwds.get("steps"):
+#             step_inputs = [{"id": id + "/" + step.id + "/" + input.id, "type":input.type} for input in step.in_ if input.source == 'UNSET']
+#             inputs = inputs + step_inputs
 
-            step_types = {}
+#             step_types = {}
 
-            print(f"run {step.run}")
-            cwl_file = cwl_parser.load_document_by_uri(step.run)
-            yaml_cwl = cwl_parser.save(cwl_file)
-            if cwl_file.class_ == "CommandLineTool":
-                clt = CommandLineTool(**yaml_cwl)
-                output_types = {output.id:output.type for output in clt.outputs}
-                step_types[step.id]= output_types
+#             print(f"run {step.run}")
+#             cwl_file = cwl_parser.load_document_by_uri(step.run)
+#             yaml_cwl = cwl_parser.save(cwl_file)
+#             if cwl_file.class_ == "CommandLineTool":
+#                 clt = CommandLineTool(**yaml_cwl)
+#                 output_types = {output.id:output.type for output in clt.outputs}
+#                 step_types[step.id]= output_types
 
-            #TODO IMPLEMENT same thing for workflow (need to recursive load types)
+#             #TODO IMPLEMENT same thing for workflow (need to recursive load types)
 
-            step_outputs = [
-                            {
-                            "id": id + "/" + step.id + "/" + output,
-                            "type": step_types[step.id][output], 
-                            "outputSource": step.id + "/" + output
-                            } for output in step.out
-                            ]
-            outputs = outputs + step_outputs
+#             step_outputs = [
+#                             {
+#                             "id": id + "/" + step.id + "/" + output,
+#                             "type": step_types[step.id][output], 
+#                             "outputSource": step.id + "/" + output
+#                             } for output in step.out
+#                             ]
+#             outputs = outputs + step_outputs
 
-        kwds.setdefault("inputs", inputs)
-        kwds.setdefault("outputs", outputs)
+#         kwds.setdefault("inputs", inputs)
+#         kwds.setdefault("outputs", outputs)
 
-        # NOTE for this to work, we would need to serialize to disk
-        # TODO CHECK if there is a better way to solve this
-        id = (Path() / id).resolve().as_uri()
+#         # NOTE for this to work, we would need to serialize to disk
+#         # TODO CHECK if there is a better way to solve this
+#         id = (Path() / id).resolve().as_uri()
 
-        self.workflow = Workflow(id,*args,**kwds, from_builder=True)
+#         self.workflow = Workflow(id,*args,**kwds, from_builder=True)
 
-    def __call__(self) -> Any:
-        workflow = self.workflow
-        # TODO Need update to pydantic v2
-        # NOTE this is probably a temporary workaround, but 
-        # we need to be able to load the original cwl when creating subworkflows.
-        # path = urlparse(workflow.id).path
+#     def __call__(self) -> Any:
+#         workflow = self.workflow
+#         # NOTE this is probably a temporary workaround, but 
+#         # we need to be able to load the original cwl when creating subworkflows.
+#         # TODO IMPLEMENT we could also accept a context object containing models of 
+#         # clts and workflows.
+#         path = urlparse(workflow.id).path
+        # serialized = workflow.model_dump_json()
         # with open(path, "w", encoding="utf-8") as file:
-        #     yaml = RootModel[Workflow](workflow).model_dump_json(indent=4)
-        #     file.write(dump(yaml))
-        return self.workflow
+        #     file.write(serialized)
+#         return self.workflow
 
-    def step():
-        pass
+#     def step():
+#         pass
 
-print(clt)
+# print(clt)
 
-# build a first step
-step_builder = StepBuilder(clt)
-step1 = step_builder()
-print(step1)
+# # build a first step
+# step_builder = StepBuilder(clt)
+# step1 = step_builder()
+# print(step1)
 
-# load a second tool
-clt_file2 = Path("/Users/antoinegerardin/Documents/projects/polus-pipelines/wic-api-v2/tests/test-data/wic/cwl_adapters/uppercase2_wic_compatible2.cwl")
-cwl_clt2 = cwl_parser.load_document_by_uri(clt_file2)
-yaml_clt2 = cwl_parser.save(cwl_clt2)
-clt2 = CommandLineTool(**yaml_clt2)
+# # load a second tool
+# clt_file2 = Path("/Users/antoinegerardin/Documents/projects/polus-pipelines/wic-api-v2/tests/test-data/wic/cwl_adapters/uppercase2_wic_compatible2.cwl")
+# cwl_clt2 = cwl_parser.load_document_by_uri(clt_file2)
+# yaml_clt2 = cwl_parser.save(cwl_clt2)
+# clt2 = CommandLineTool(**yaml_clt2)
 
-# build our second step
-step_builder2 = StepBuilder(clt2)
-step2 = step_builder2()
-print(step2)
+# # build our second step
+# step_builder2 = StepBuilder(clt2)
+# step2 = step_builder2()
+# print(step2)
 
-# NOTE that is simulating the linking between 2 steps ios.
-echo_out_message_string = step1.out[0]
-uppercase_in_message = step2.in_[0]
-uppercase_in_message.source = step1.id + "/" + echo_out_message_string
+# # NOTE that is simulating the linking between 2 steps ios.
+# echo_out_message_string = step1.out[0]
+# uppercase_in_message = step2.in_[0]
+# uppercase_in_message.source = step1.id + "/" + echo_out_message_string
 
-echo_out_message_string = step1.out[0]
-uppercase_message_in_message = step2.in_[1]
-uppercase_message_in_message.source = step1.id + "/" + echo_out_message_string
-
-
-
-workflow_file= Path("/Users/antoinegerardin/Documents/projects/polus-pipelines/wic-api-v2/tests/test-data/cwl/workflow7.cwl")
-cwl_wf1 = cwl_parser.load_document_by_uri(workflow_file)
-yaml_wf1 = cwl_parser.save(cwl_wf1)
-wf1 = Workflow(**yaml_wf1)
-print(wf1)
-
-wf2_builder = WorkflowBuilder("wf3", steps=[step1, step2])
-wf3 = wf2_builder()
-print(wf3)
-
-step_builder3 = StepBuilder(wf3)
-step3 = step_builder3()
-print(step3)
+# echo_out_message_string = step1.out[0]
+# uppercase_message_in_message = step2.in_[1]
+# uppercase_message_in_message.source = step1.id + "/" + echo_out_message_string
 
 
 
-# wf4_builder = WorkflowBuilder("wf4", steps = [step3])
-# step4 = wf4_builder()
-# print(step4)
+# workflow_file= Path("/Users/antoinegerardin/Documents/projects/polus-pipelines/wic-api-v2/tests/test-data/cwl/workflow7.cwl")
+# cwl_wf1 = cwl_parser.load_document_by_uri(workflow_file)
+# yaml_wf1 = cwl_parser.save(cwl_wf1)
+# wf1 = Workflow(**yaml_wf1)
+# print(wf1)
+
+# wf2_builder = WorkflowBuilder("wf3", steps=[step1, step2])
+# wf3 = wf2_builder()
+# print(wf3)
+
+# step_builder3 = StepBuilder(wf3)
+# step3 = step_builder3()
+# print(step3)
+
+
+
+# # wf4_builder = WorkflowBuilder("wf4", steps = [step3])
+# # step4 = wf4_builder()
+# # print(step4)
 
 
 
