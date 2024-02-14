f10387eba401d9904d82b344f2d37f1dabd75e34
agerardin <antoine.gerardin@gmail.com>
2024-02-14 09:11:04 -0500

    tests: workflows and subworkflows basic tests.

--- tests/workflows/test_workflow_builder.py
+++ tests/workflows/test_workflow_builder.py
@@ -54,3 +54,40 @@ def test_workflow_builder_with_linked_steps(test_data_dir: Path, clts: list[str]
     input_count = len(wf.inputs)
     expected_count = 1
     assert input_count == expected_count, f"workflow should have {expected_count} input, got {input_count}." 
+
+
+@pytest.mark.parametrize("clts", [["echo_string.cwl", "uppercase2_wic_compatible2.cwl", "touch_single.cwl"]])
+def test_workflow_builder_with_subworkflows(test_data_dir: Path, clts: list[str]):
+    steps = []
+    for filename in clts:
+        cwl_file = test_data_dir / filename
+        clt = CommandLineTool.load(cwl_file)
+        print(clt)
+        step_builder = StepBuilder(clt)
+        step = step_builder()
+        print(step)
+        steps.append(step)
+
+    (step1, step2, step3) = steps
+    step2.message = step1.message_string
+    step2.uppercase_message = step1.message_string
+
+    wf_builder = WorkflowBuilder("wf3", steps=steps)
+    wf: Workflow = wf_builder()
+    print(wf)
+    step_builder = StepBuilder(wf)
+    step12 = step_builder()
+    print(step12)
+
+    step3.touchfiles = step12.wf3___step_uppercase2_wic_compatible2___uppercase_message
+    wf_builder = WorkflowBuilder("wf4", steps = [step12, step3])
+    wf4 = wf_builder()
+
+    step_builder = StepBuilder(wf4)
+    step4 = step_builder()
+    print(wf4)
+
+    step4.wf4___step_wf3___wf3___step_echo_string___message = "ok"
+    config = step4.save_config()
+
+    print(config)
